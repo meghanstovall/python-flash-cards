46d7e6a42879ec362cf7f1022e48be543ad71e0b
Meghan Stovall <meghanstovall15@augustana.edu>
2021-01-11 07:45:45 -0700

    Finish card

---
+++ lib/Card.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 2020
+
+@author: Meghan Stovall
+"""
+
+class Card:
+
+    def __init__(self, question, answer, category):
+        self.question = question
+        self.answer = answer
+        self.category = category
